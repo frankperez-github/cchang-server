2461902b95afcf7ee883d9ad4b6db326c463103a
Frank Pérez <fp848584@gmail.com>
2023-04-18 12:41:52 -0400

    changing settings

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 const jsonServer = require('json-server')
-const clone = require('clone') 
-const data = require('./db.json')
 
-const isProductionEnv = process.env.NODE_ENV === 'development';
 const server = jsonServer.create()
 
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-server.use(middlewares)
-
-server.use((req, res, next) => {
-    if (req.path !== '/')
-        router.db.setState(router.db)
-    next()
-})
-
 server.use(router)
+
+
 server.listen(process.env.PORT || 8000, () => {
     console.log('JSON Server is running')
 })
