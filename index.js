2976126cbb03090057fa0e186eee855d0075e521
Frank Pérez <fp848584@gmail.com>
2023-04-18 13:09:49 -0400

    changing settings

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const isProductionEnv = process.env.NODE_ENV === 'development';
 const server = jsonServer.create()
 
 // For mocking the POST request, POST request won't make any changes to the DB in production environment
-// const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
-//     _isFake: isProductionEnv
-// })
-const router = jsonServer.router('db.json')
+const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
+    _isFake: !isProductionEnv
+})
+// const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
