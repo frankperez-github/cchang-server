7d967c47b16b277acbb9c7835a4e34f05d5dfd36
frankperez-github <fp848584@gmail.com>
2023-09-03 23:17:41 -0400

    connecting front end and API

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const dotenv = require("dotenv").config()
 const express = require("express")
 const connectDB = require("./config/dbConnection")
+const cors = require("cors")
 
 const port = process.env.PORT || 5000
 
 connectDB()
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(`/projects`, require("./routes/projectsRoutes"))
 
