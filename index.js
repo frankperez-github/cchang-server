1126cd96e83a48ebf6e887d5298c5f109ca395e4
frankperez-github <fp848584@gmail.com>
2023-09-04 21:21:07 -0400

    connecting front end and API

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ connectDB()
 const app = express()
 
 app.use(cors({
-    origin: ['http://localhost:3000/', 'https://cchang.vercel.app/', 'https://cchang-server.vercel.app/']
+    origin: ['http://localhost:3000', 'https://cchang.vercel.app', 'https://cchang-server.vercel.app']
 }))
 app.use(express.json())
 app.use(`/projects`, require("./routes/projectsRoutes"))
